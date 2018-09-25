ab781d763d2db909476777d03a22b920784f7f31
Blues <i@iblues.name>
2018-09-25 16:32:54 +0800

    [fix]完善nav配置表

--- src/Copy/config/Schemas/Components/Admin/Nav.php
+++ src/Copy/config/Schemas/Components/Admin/Nav.php
@@ -4,59 +4,60 @@
  * 也可以自己指定
  */
 return [
-    'detail'=>[
-        'table'=>[
-            'fields'=>[
+    'detail' => [
+        'table' => [
+            'search' => ['parent_id', 'name'],
+            'fields' => [
                 'id',
-            'catid',
-            'name',
-            'url',
-            'class',
+//            'catid',
+                'name',
+                'url',
+                'class',
 //            'module',
-            'parent_id',
-            'ranking',
-            'status',
+                'parent_id',
+                'ranking',
+                'status',
 //            'created_at',
 //            'updated_at'
-             ],
-        ],
-        'add'=>[
-            'fields'=>[
-                'catid',
-            'name',
-            'url',
-            'class',
-            'module',
-            'parent_id',
-            'ranking',
-            'status'
-             ],
-        ],
-        'edit'=>[
-            'fields'=>[
-                'catid',
-            'name',
-            'url',
-            'class',
-            'module',
-            'parent_id',
-            'ranking',
-            'status'
             ],
         ],
-        'detail'=>[
-            'fields'=>[
+        'add' => [
+            'fields' => [
+                'catid',
+                'name',
+                'url',
+                'class',
+                'module',
+                'parent_id',
+                'ranking',
+                'status'
+            ],
+        ],
+        'edit' => [
+            'fields' => [
+                'catid',
+                'name',
+                'url',
+                'class',
+                'module',
+                'parent_id',
+                'ranking',
+                'status'
+            ],
+        ],
+        'detail' => [
+            'fields' => [
                 'id',
-            'catid',
-            'name',
-            'url',
-            'class',
-            'module',
-            'parent_id',
-            'ranking',
-            'status',
-            'created_at',
-            'updated_at'
+                'catid',
+                'name',
+                'url',
+                'class',
+                'module',
+                'parent_id',
+                'ranking',
+                'status',
+                'created_at',
+                'updated_at'
             ],
         ],
     ],
