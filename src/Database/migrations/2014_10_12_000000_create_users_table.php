1707d3a52bc72182b8ff3115c4d0773e592926da
Blues <i@iblues.name>
2018-10-04 23:26:48 +0800

    [feat]子账号

--- src/Database/migrations/2014_10_12_000000_create_users_table.php
+++ src/Database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('common_user', function (Blueprint $table) {
-            $table->increments('id')->comment('编号');
-            $table->string('type',10)->comment('用户类型')->default('customer');
+            $table->bigIncrements('id')->comment('编号');
+
+            $table->unsignedBigInteger('belong_id')->comment('从属企业')->default(0);//子账号
+//            $table->string('type',10)->comment('用户类型')->default('customer');
+
             $table->string('name')->comment('备注');
             $table->string('api_token', 64)->unique();
             $table->string('first_site')->comment('首次注册城市')->default(1);
