2c9c449895f9ec683bbf2583ae3aead5a1fde7aa
Blues <i@iblues.name>
2018-10-31 20:46:46 +0800

    [feat]时间戳

---
+++ src/Components/Field/Timestamp.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/25
+ * Time: 21:41
+ */
+
+namespace Larfree\Components\Field;
+use Larfree\Components\Components;
+
+class Timestamp extends Components
+{
+    /**
+     * 生成缩略图
+     * @param $config
+     * @param $array
+     */
+    static public function getAttribute($config,&$array){
+        $value  = $array[$config['key']];
+        $array[$config['key'] . '_timestamp'] = strtotime($value);
+    }
+}
