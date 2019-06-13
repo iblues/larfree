6b38aedb3bbc324a4711be560011ab80bdf20a2d
Blues <i@iblues.name>
2019-06-13 18:25:37 +0800

    完善报错时候的消息提醒

--- src/Libs/ApiSchemas.php
+++ src/Libs/ApiSchemas.php
@@ -86,13 +86,13 @@ class ApiSchemas extends Schemas
             foreach ($schemas as $item) {
                 if (isset($item['rule'])) {
                     $validate[$item['key']] = self::formatFieldValidate($item['rule']);
+                    $validateName[$item['key']] = $item['name'];
                 }
             }
         }
-
 //        exit();
         //最后处理下格式,方便让laravel直接调用
-        $validate = self::formatValidate(array_filter($validate),$schemas);
+        $validate = self::formatValidate(array_filter($validate),$schemas,$validateName);
         return $validate;
     }
 
@@ -124,20 +124,20 @@ class ApiSchemas extends Schemas
      */
     static protected function formatFieldValidate($validate){
 
-            $newValidate=[];
-            if(is_string($validate)){
-                $newValidate[$validate]='';
-            }elseif(is_array($validate)) {
+        $newValidate=[];
+        if(is_string($validate)){
+            $newValidate[$validate]='';
+        }elseif(is_array($validate)) {
 
-                array_walk($validate, function (&$item,&$key)use(&$newValidate) {
-                    if(is_numeric($key)){
-                        $key = $item;
-                        $item = '';
-                    }
-                    $newValidate[$key]=$item;
-                });
-            }
-            return $newValidate;
+            array_walk($validate, function (&$item,&$key)use(&$newValidate) {
+                if(is_numeric($key)){
+                    $key = $item;
+                    $item = '';
+                }
+                $newValidate[$key]=$item;
+            });
+        }
+        return $newValidate;
     }
 
     /**
@@ -147,10 +147,10 @@ class ApiSchemas extends Schemas
      * @param $method
      * @return array
      */
-    static protected function formatValidate($defValidate,$fields){
+    static protected function formatValidate($defValidate,$fields,$name=[]){
         $messages = [];
         //处理格式,自定义消息
-        array_walk($defValidate,function(&$item,$key)use(&$messages){
+        array_walk($defValidate,function(&$item,$key)use(&$messages,$fields,$name){
             $rule = array_keys($item);
             foreach($item as $k=>$v){
                 if($v) {
@@ -158,8 +158,16 @@ class ApiSchemas extends Schemas
                     $messages[$key .'.' .$k] = $v;
                 }
             }
+
+
+            $name = array_get($name,$key);
+            if(!$name)
+                $name = array_get($name,$key)['name'];
+            self::replaceLocalValidateMsg($rule,$key,$name,$messages);
+
             $rule = implode('|',$rule);
             $item = $rule;
+
         });
         //过滤rule为空的
         $defValidate = array_filter($defValidate,function($item){
@@ -168,8 +176,29 @@ class ApiSchemas extends Schemas
             else
                 return false;
         });
+
         $newValidate=['rules'=>$defValidate,'msg'=>$messages];
         return $newValidate;
+    }
+
+    /**
+     * 替换效验的标题名
+     * @param $rule
+     * @param $name
+     * @param $messages
+     */
+    static protected function replaceLocalValidateMsg($rule,$key,$name,&$messages){
+
+        //新增自定义验证
+        foreach ($rule as $r){
+
+            if(is_string($r) && stripos($r,'|')){
+                $r = explode('|',$r);
+                self::replaceLocalValidateMsg($r,$key,$name,$messages);
+            }else if($r && app('translator')->has('validation.'.$r) ){
+                $messages[$key .'.' .$r] =  str_ireplace(':attribute',$name,app('translator')->get('validation.'.$r));
+            }
+        }
     }
 
 }
