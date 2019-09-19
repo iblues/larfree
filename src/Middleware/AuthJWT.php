5ee445fbfc50417ddba48feaaa8ec9d94d48d0d8
Blues <i@iblues.name>
2019-09-19 22:18:23 +0800

    改造jwt中间件

--- src/Middleware/AuthJWT.php
+++ src/Middleware/AuthJWT.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace Larfree\Middleware;
 
 use Closure;
 use App\Models\Common\CommonUser;
@@ -21,7 +21,7 @@ class AuthJWT
      */
     public function handle($request, Closure $next, int $is_force = 1)
     {
-        // 解析token
+        // 开发者模式
         if (env('DEF_USER')) {
             $user = CommonUser::find(getLoginUserID());
             app()->current_auth_user = $user;// 放入实例中，同请求内复用
@@ -29,15 +29,15 @@ class AuthJWT
         } else {
             try {
                 if (!$user = JWTAuth::parseToken()->authenticate()) {
-                    apiError('user not found', '', 401);
+                    apiError('user not found', '', 401.01);
                 }
                 app()->current_auth_user = $user;// 放入实例中，同请求内复用
             } catch (TokenExpiredException $e) {
                 if($is_force)
-                    apiError('登录已经过期，请重新登录', null, 401);
+                    apiError('登录已经过期，请重新登录', null, 401.02);
             } catch (TokenInvalidException $e) {
                 if($is_force)
-                    apiError('登录已经过期，请重新登录', null, 401);
+                    apiError('登录已经过期，请重新登录', null, 401.03);
             } catch (JWTException $e) {
                 if($is_force)
                     apiError('请登录', null, 401);
