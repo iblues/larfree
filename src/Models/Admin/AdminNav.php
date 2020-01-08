d964ab4a764360753efba904ac919af33b569a22
Blues <i@iblues.name>
2020-01-08 17:31:17 +0800

    简化仓库

--- src/Models/Admin/AdminNav.php
+++ src/Models/Admin/AdminNav.php
@@ -13,29 +13,25 @@ class AdminNav extends Api{
     }
 
     /**
-     * 以数组的方式获取所有菜单
-     * @param int $catID 菜单分类id
-     * @param int $parent_id 菜单id
-     * @return mixed
+     * 返回后台的目录.
+     * @author Blues
+     * @param $model
+     * @param int $tree 树状结构
+     * @param $onlyStatus=1 只要开启的
+     * @return array
      */
-    public function getAllMenu($parent_id = 0,$catID = '') {
-        $where['status'] = 1;
-        $where['parent_id'] = $parent_id;
-        if($catID)
-            $where['catid'] = $catID;
-        $menus = $this->where($where)->orderBy('ranking' ,'desc')->get()->toArray();
-        if (!empty($menus)) {
-            foreach ($menus as $key => $menu) {
-                $menu_child = $this->getAllMenu($menu['id']);
-                if (!empty($menu_child)) {
-                    //子菜单不为空放在 child 数组中
-                    $menus[$key]['child']= $menu_child;
-                }else{
-                    $menus[$key]['child']= "";
-                }
-            }
-        }
-        return $menus;
+    static public function getTreeNav($model,$tree=1,$onlyStatus=1){
+
+        if($onlyStatus)
+            $model->where('status',1);
+
+        $nav = $model->orderBy('ranking','desc')->get();
+        $nav = $nav->toArray();
+//        $return  = event(new FilterNavEvent($nav,$model::class));
+        if(!$tree)
+            return $nav;
+        return  listToTree($nav, 'id', 'parent_id', 'children');
+
     }
 
 
