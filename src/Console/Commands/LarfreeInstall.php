0443fd6c8471401212e529c86c4361d164abe433
Blues <i@iblues.name>
2020-03-19 17:44:50 +0800

    install

--- src/Console/Commands/LarfreeInstall.php
+++ src/Console/Commands/LarfreeInstall.php
@@ -2,10 +2,13 @@
 
 namespace Larfree\Console\Commands;
 
-use App\Models\Admin\AdminNav;
 use App\Models\Common\CommonUser;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Event;
 use Larfree\Libs\Make;
+use Larfree\Models\Admin\AdminNav;
+use LarfreePermission\Models\User\UserAdmin;
+use function GuzzleHttp\Psr7\str;
 
 class LarfreeInstall extends Command
 {
@@ -40,34 +43,45 @@ class LarfreeInstall extends Command
         $replace = $arguments['replace'];
         $this->createAdmin();
         $this->createNav();
+
+        //广播事件. 方便插件
+        Event::dispatch('larfree.install');
+        echo 'success!';
     }
 
-    private function createNav(){
-        $nav=[
-            'name'=>'测试',
-            'url'=>'/curd/test.test/',
-            'class'=>'',
-            'module'=>'',
-            'status'=>1,
+    private function createNav()
+    {
+        $nav = [
+            'name' => '测试',
+            'url' => '/curd/test.test/',
+            'class' => '',
+            'module' => '',
+            'status' => 1,
         ];
-        AdminNav::insert($nav);
-        $nav=[
-            'name'=>'用户管理',
-            'url'=>'/curd/common.user/',
-            'class'=>'',
-            'module'=>'',
-            'status'=>1,
+        AdminNav::firstOrCreate(['url'=>$nav['url']],$nav);
+        $nav = [
+            'name' => '用户管理',
+            'url' => '/curd/common.user/',
+            'class' => '',
+            'module' => '',
+            'status' => 1,
         ];
-        AdminNav::insert($nav);
+        AdminNav::firstOrCreate(['url'=>$nav['url']],$nav);
     }
 
-    private function createAdmin(){
-        (new CommonUser)->create(['name'=>'admin',
-            'phone'=>'18008010521',
-            'email'=>'i@iblues.name',
-            'api_token'=>str_random(30),
-            'password'=>'123',
-        ]);
+    private function createAdmin()
+    {
+        $user = (new CommonUser)->firstOrCreate(
+            ['email' => 'i@Iblues.name'],
+            [
+                'name' => 'admin',
+                'phone' => '13888888888',
+                'email' => 'i@Iblues.name',
+                'password' => '123',
+            ]);
+
+
+        Event::dispatch('larfree.install.admin',['user'=>$user]);
     }
 
 }
