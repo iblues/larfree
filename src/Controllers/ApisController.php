80814aeabd4ba321a49322ff028ef03e7791cb1b
Blues <i@iblues.name>
2020-04-07 16:14:21 +0800

    unique处理

--- src/Controllers/ApisController.php
+++ src/Controllers/ApisController.php
@@ -15,6 +15,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Auth;
+use Illuminate\Validation\Rule;
 use Larfree\Libs\ApiSchemas;
 use Larfree\Resources\ApiResource;
 use Illuminate\Support\Facades\DB;
@@ -61,13 +62,13 @@ class ApisController extends BaseController
 
     /**
      * 分页列表
-     * @author Blues
      * @param Request $request
      * @return mixed
      * @throws \Exception
      * @ATU\Api(
      *     @ATU\RouteIgnore()
      * )
+     * @author Blues
      */
     public function index(Request $request)
     {
@@ -106,25 +107,25 @@ class ApisController extends BaseController
 
     /**
      * 更新
-     * @author Blues
      * @param Request $request
      * @param $id
      * @return mixed
      * @throws \Exception
+     * @author Blues
      */
     public function update(Request $request, $id)
     {
-        return $this->service->link()->updateOne($request->all(),$id);
+        return $this->service->link()->updateOne($request->all(), $id);
     }
 
 
     /**
      * 删除
-     * @author Blues
      * @param $id
      * @param Request $request
      * @return string|void
      * @throws \Exception
+     * @author Blues
      */
     public function destroy($id, Request $request)
     {
@@ -134,8 +135,8 @@ class ApisController extends BaseController
 
     /**
      * 设置msg
-     * @author Blues
      * @param string $title
+     * @author Blues
      */
     protected function setMsg(string $title)
     {
@@ -146,36 +147,76 @@ class ApisController extends BaseController
     /**
      * 获取验证规则. put的时候 会自动加上sometimes
      * 接口文档那边要用 所以暂时设置为public
-     * @author Blues
      * @param $method 函数名
      * @param string $httpMethod http方法
+     * @param Request $request
      * @return array
      * @throws \Exception
+     * @author Blues
      */
-    public function getValidation($method, $httpMethod = 'POST')
+    public function getValidation($method, $httpMethod = 'POST', $param)
     {
         $ext = isset($this->in[$method]) ? $this->in[$method] : ['*'];
         $validate = ApiSchemas::getValidate($this->modelName, 'in', $ext);
         //PUT修改的,不一定是所有字段都有,所以自动加上sometimes
-        if ($httpMethod == 'PUT' && $method == 'update') {
-            array_walk($validate['rules'], function (&$value) {
+        array_walk($validate['rules'], function (&$value) use ($param, $httpMethod, $method) {
+            //方法是Put或者方法名中包含update的
+            if ($httpMethod == 'PUT' || stripos($method, 'update') !== false) {
                 if (stripos($value, 'sometimes') === false) {
                     $value = 'sometimes|' . $value;
                 }
-            });
+            }
 
-        }
+            $value = explode('|', $value);
+            //处理unique的问题.
+            // 'unique:user_admin,user_id,数据库主键key,变量Key' => '该用户已添加,请勿重复添加'
+            // 'unique:user_admin,user_id' => '该用户已添加,请勿重复添加'
+            array_walk($value, function (&$value) use ($param) {
+                if (stripos($value, 'unique:') === 0) {
+                    //默认先用最后一个参数.
+                    $id = end($param);
+                    if (is_object($id)) {
+                        $id = null;
+                    }
+
+                    $key = explode(',', $value);
+                    //用unique中的指定参数
+                    if (isset($key['3'])) {
+                        $request = $param[0];
+                        //优先用body中的id
+                        $id = $request->get($key[3]);
+                        //其次用path的
+                        if (!$id) {
+                            $id = $param[$key['3']];
+                        }
+                    }
+
+                    $ignoreKey = $key['2'] ?? 'id';
+                    if (!$key[1]) {
+                        apiError('unique miss key example: unique:table_name,filed_key_name');
+                    }
+
+                    //有忽略规则的才继续
+                    if ($id) {
+                        $value = Rule::unique('user_admin', $key['1'])->ignore($id, $ignoreKey);
+                    } else {
+                        $value = Rule::unique('user_admin', $key['1']);
+                    }
+
+                }
+            });
+        });
         return $validate;
     }
 
     /**
      * $in的定义
      * 接口文档可能会用,所以public
-     * @author Blues
      * @param $method
      * @param string $group
      * @return array
      * @throws \Exception
+     * @author Blues
      */
     public function getParamDefine($method, $group = 'in')
     {
@@ -186,18 +227,18 @@ class ApisController extends BaseController
 
     /**
      * 对输入字段过滤 , in字段为字段明细
-     * @author Blues
      * @param $request
      * @param $group
      * @param $method
      * @throws \Exception
+     * @author Blues
      */
     protected function filterInput(&$request, $group, $method)
     {
         $ext = isset($this->in[$method]) ? $this->in[$method] : [];
         //字段过滤
         $fields = ApiSchemas::getApiAllowField($this->modelName, 'in', $method, $ext);
-        if ($fields != false && !array_key_exists('*',$fields)) {
+        if ($fields != false && !array_key_exists('*', $fields)) {
             $data = array_diff_key($request->all(), array_flip(array_keys($fields)));
             foreach ($data as $k => $v) {
                 $request->offsetUnset($k);
@@ -208,11 +249,11 @@ class ApisController extends BaseController
 
     /**
      * 魔术回调
-     * @author Blues
      * @param $method
      * @param $parameters
      * @return \Illuminate\Support\Collection|ApiResource|mixed
      * @throws \Exception
+     * @author Blues
      */
     public function callAction($method, $parameters)
     {
@@ -221,12 +262,12 @@ class ApisController extends BaseController
          * 进行输入参数的验证和过滤
          * //当参数存在,并且他是Request 而且不是Get. get就不做参数验证了
          */
-//        dd($parameters[0]);
         if (isset($parameters[0]) && $parameters[0] instanceof Request && ($parameters[0]->getMethod() != 'GET' && $parameters[0]->getMethod() != 'DELETE')) {
             $this->filterInput($parameters[0], 'in', $method);
-            $validate = $this->getValidation($method, $parameters[0]->getMethod());
+            $validate = $this->getValidation($method, $parameters[0]->getMethod(), $parameters);
             $this->validate($parameters[0], $validate['rules'], $validate['msg']);
         }
+
 
         //执行真实的函数
         $return = call_user_func_array([$this, $method], $parameters);
