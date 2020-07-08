6a48883ff3bfb4034647daabffce97ab71c9d2f6
Blues <i@iblues.name>
2020-07-08 16:47:52 +0800

    调整缩略图大小

--- src/Components/Field/Image.php
+++ src/Components/Field/Image.php
@@ -26,9 +26,6 @@ class Image extends Components
         }
         $value  = $array[$config['key']];
         if(is_array($array)) {
-//            $array[$config['key'] . '_small'] = getArrayThumb($value, '200', '200');
-//            $array[$config['key'] . '_big'] = getArrayThumb($value, '1000', '1000');
-//            $array[$config['key'].'_origin'] = getArrayThumb($value,'0','0',-1);
             //上面的慢慢淘汰
             $array[$config['key'].'_link']=[
                 'small'=> getArrayThumb($value, '200', '200'),
@@ -36,9 +33,6 @@ class Image extends Components
                 'large'=>getArrayThumb($value,'1500','4000'),
             ];
         }else{
-//            $array[$config['key'] . '_small'] = getThumb($value, '200', '200');
-//            $array[$config['key'].'_big'] = getThumb($value,'1000','1000');
-//            $array[$config['key'].'_origin'] = getThumb($value,'0','0',-1);
             //上面的慢慢淘汰
             $array[$config['key'].'_link']=[
                 'small'=> getThumb($value, '200', '200'),
