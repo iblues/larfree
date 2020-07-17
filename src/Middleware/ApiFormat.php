9fc4e56845c11f859238c0dc69985d0da6c6b2ea
Blues <i@iblues.name>
2020-07-17 15:45:20 +0800

    优化中间件

--- src/Middleware/ApiFormat.php
+++ src/Middleware/ApiFormat.php
@@ -35,37 +35,23 @@ class ApiFormat
 
         $content = $response->getOriginalContent();
 
+        //设置中文不要转码
         if (method_exists($response, 'setEncodingOptions')) {
             $response->setEncodingOptions(JSON_UNESCAPED_UNICODE);
         }
 
-//dd($content);
-        $json = json_decode($response->getContent(), true);
+        $this->setHttpCode(null,$response);
+        $this->appCode($request,$response);
 
-        if (isset($json['code'])) {
-            $this->setHttpCode($json['code'], $response);
-            $this->appCode($request, $response);
-
-            return $response;
-        }
-        //没数据的时候
-//        if( $content['message']=='Object of class Illuminate\Database\Eloquent\Builder could not be converted to string'){
-//            $content=[];
-//            $response->setStatusCode(200);
-//        }
-        if (!$request->ajax()) {
-            return $response;
-        }
-        //200代码的才是正常返回
-        $code = $response->getStatusCode() < 400 ? 1 : 0;
-
-        //对分页进行再处理
-        $page     = $this->FormatPage($content);
-        $response = $this->FormatJson($response, $content, $page, $code);
-        $this->appCode($request, $response);
         return $response;
     }
 
+    /**
+     * app不方便处理其他异常状态码
+     * @param $request
+     * @param $response
+     * @author Blues
+     */
     protected function appCode($request, $response)
     {
         if ($request->headers->get('device') == 'app' && $response->getStatusCode() < 500) {
@@ -73,87 +59,17 @@ class ApiFormat
         }
     }
 
-    protected function setHttpCode($code, $response)
+    protected function setHttpCode($code = null,$response)
     {
+        if(!$code)
+            $code = $response->getStatusCode();
         if ($code > 10000) {
             $code = intval($code / 100);
         }
-
         if ($code >= 600) {
             $code = 412;
         }
-
         $response->setStatusCode($code);
     }
 
-    /**
-     * 重置json格式
-     * @param $response
-     * @param $content
-     * @param  int  $code
-     * @return mixed
-     */
-    protected function FormatJson($response, $content, $page, $code = 1)
-    {
-        $StatusCode = $response->getStatusCode();
-        $msg        = '';
-        //ios需要返回200才能解析
-//        if($StatusCode!=500)
-//            $response->setStatusCode(200);
-
-        if ($StatusCode == 302) {
-            return $response;
-        }
-        if ($StatusCode == 422 && isset($content['errors'])) {
-            $msg     = current(current($content['errors']));
-            $content = $content['errors'];
-        }
-
-        //兼容不同版本的validate返回
-        if ($StatusCode == 422 && !$msg) {
-            $msg     = current(current($content));
-            $content = $content;
-        }
-
-//        if($StatusCode==401)
-//            $code=-10;
-
-        //重新设置格式
-        if (method_exists($response, 'setData')) {
-            //如果是json响应
-            return $response->setData([
-                'msg' => $msg,
-                'code' => $StatusCode,
-                'status' => $code,
-                'data' => $content,
-                'debug' => '',
-            ]);
-        } else {
-            //视图类响应
-            return $response->setContent([
-                'code' => $StatusCode,
-                'status' => $code,
-                'data' => $content,
-                'debug' => '',
-            ]);
-        }
-    }
-
-    /**
-     * 如果是分页类,重构下结果
-     * @param &$data
-     * @return array 分页数组
-     */
-    protected function FormatPage(&$data)
-    {
-        //不是分页类
-        if (!is_subclass_of($data, '\Illuminate\Pagination\AbstractPaginator')) {
-            return [];
-        }
-        $data = $data->toArray();
-        $page = $data;
-        $data = $data['data'];
-        unset($page['data']);
-        return $page;
-    }
 }
