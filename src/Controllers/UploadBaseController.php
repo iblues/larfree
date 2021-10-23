2a61e59a197d2c6c93d3f3d6db99b787f7267a09
osindex <yaoiluo@gmail.com>
2021-10-23 20:57:19 +0800

    :art: 1.macro apiResource 新版本无效; 2.上传调整

--- src/Controllers/UploadBaseController.php
+++ src/Controllers/UploadBaseController.php
@@ -2,7 +2,6 @@
 
 namespace Larfree\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 //use Auth;
@@ -17,30 +16,30 @@ class UploadBaseController extends ApisController
      */
     public function images(Request $request)
     {
-        $type   = config('filesystems.file_type', 'local');
+        $type = config('filesystems.file_type', 'local');
         $module = $request->get('module', 'images');
         switch ($type) {
             case 'qiniu':
-                $disk     = \Storage::disk('qiniu'); //使用七牛云上传
-                $time     = $module.'/'.date('Y-m-d');//上传目录
-                $filename = $disk->put($time, $request->file('file'));//上传
+                $disk = \Storage::disk('qiniu'); //使用七牛云上传
+                $time = $module . '/' . date('Y-m-d'); //上传目录
+                $filename = $disk->put($time, $request->file('file')); //上传
                 break;
             case 'oss':
-                $disk     = \Storage::disk('oss'); //使用oss
-                $time     = $module.'/'.date('Y-m-d');//上传目录
-                $filename = $disk->put($time, $request->file('file'));//上传
+                $disk = \Storage::disk('oss'); //使用oss
+                $time = $module . '/' . date('Y-m-d'); //上传目录
+                $filename = $disk->put($time, $request->file('file')); //上传
                 break;
-            default :
-                $disk     = \Storage::disk('public');
-                $time     = $module.'/'.date('Y-m-d');
+            default:
+                $disk = \Storage::disk('public');
+                $time = $module . '/' . date('Y-m-d');
                 $filename = $disk->putFile($time, $request->file('file'), 'public');
                 break;
         }
         if (!$filename) {
             apiError('上传失败', 500);
         }
-        $small_url  = getThumb($filename, '200', '200', 0);
-        $big_url    = getThumb($filename, '1000', '1000', 0);
+        $small_url = getThumb($filename, '200', '200', 0);
+        $big_url = getThumb($filename, '1000', '1000', 0);
         $origin_url = $disk->url($filename); //原图
         return ['small_url' => $small_url, 'name' => $filename, 'big_url' => $big_url, 'origin_url' => $origin_url];
     }
@@ -53,31 +52,23 @@ class UploadBaseController extends ApisController
      */
     public function files(Request $request)
     {
-        $type   = config('filesystems.file_type', 'local');
+        $type = config('filesystems.file_type', 'local');
         $module = $request->get('module', 'files');
         switch ($type) {
             case 'qiniu':
-                $disk     = \Storage::disk('qiniu'); //使用七牛云上传
-                $time     = $module.'/'.date('Y-m-d');//上传目录
-                $file     = $request->file('file');
-                $name     = time().'.'.$file->getClientOriginalExtension();
-                $filename = $disk->putFileAs($time, $file, $name);//上传
+                $disk = \Storage::disk('qiniu'); //使用七牛云上传
                 break;
             case 'oss':
-                $disk     = \Storage::disk('oss'); //使用oss
-                $time     = $module.'/'.date('Y-m-d');//上传目录
-                $file     = $request->file('file');
-                $name     = time().'.'.$file->getClientOriginalExtension();
-                $filename = $disk->putFileAs($time, $file, $name);//上传
+                $disk = \Storage::disk('oss'); //使用oss
                 break;
-            default :
-                $disk     = \Storage::disk('public');
-                $time     = $module.'/'.date('Y-m-d');//上传目录
-                $file     = $request->file('file');
-                $name     = time().'.'.$file->getClientOriginalExtension();
-                $filename = $disk->putFileAs($time, $file, $name);//上传
+            default:
+                $disk = \Storage::disk('public');
                 break;
         }
+        $time = $module . '/' . date('Y-m-d'); //上传目录
+        $file = $request->file('file');
+        $name = time() . '.' . $file->getClientOriginalExtension();
+        $filename = $disk->putFileAs($time, $file, $name); //上传
         if (!$filename) {
             apiError('上传失败', 500);
         }
